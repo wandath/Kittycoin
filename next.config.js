157b7d24c23f5e9c7ed4a58d04fe279fc575dbe5
wandath <anya.schouvey@ecole-hexagone.com>
2023-06-07 09:55:44 +0200

    updated

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['thecraftchop.com', 'www.svgrepo.com'],
+  },
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     return config;
